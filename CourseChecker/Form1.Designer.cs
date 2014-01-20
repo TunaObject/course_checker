af9815470b0a01e085357fa346eaa31557199b1f
ahageali <abdul.hageali+github@gmail.com>
2014-01-20 16:45:47 -0500

    Initial commit

---
+++ CourseChecker/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace SwagBot3000
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCourseCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStudentId = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(963, 455);
+            this.webBrowser1.TabIndex = 5;
+            this.webBrowser1.Url = new System.Uri("https://quest.pecs.uwaterloo.ca/psp/SS/ACADEMIC/SA/", System.UriKind.Absolute);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 473);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Activate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 12);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(963, 484);
+            this.txtLog.TabIndex = 7;
+            this.txtLog.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 478);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Course Code:";
+            // 
+            // txtCourseCode
+            // 
+            this.txtCourseCode.Location = new System.Drawing.Point(190, 475);
+            this.txtCourseCode.Name = "txtCourseCode";
+            this.txtCourseCode.Size = new System.Drawing.Size(100, 20);
+            this.txtCourseCode.TabIndex = 9;
+            this.txtCourseCode.Text = "ECON 102";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(381, 478);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Student ID:";
+            // 
+            // txtStudentId
+            // 
+            this.txtStudentId.Location = new System.Drawing.Point(448, 475);
+            this.txtStudentId.Name = "txtStudentId";
+            this.txtStudentId.Size = new System.Drawing.Size(100, 20);
+            this.txtStudentId.TabIndex = 11;
+            this.txtStudentId.Text = "20xxxxxx";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(723, 475);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(252, 20);
+            this.txtEmail.TabIndex = 12;
+            this.txtEmail.Text = "example@example.com";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(682, 478);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Email:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 508);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtStudentId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCourseCode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Course Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCourseCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtStudentId;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CourseChecker/Form1.cs
@@ -0,0 +1,124 @@
+﻿//Made by Abdul Hage-ali
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace SwagBot3000
+{
+    public partial class Form1 : Form
+    {
+        string courseCode;
+        string studentID;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LogWrite(string message)
+        {
+            string formatted = DateTime.UtcNow.ToShortTimeString() + " " + message + Environment.NewLine;
+            if (txtLog.MaxLength <= (txtLog.Text + formatted).Length)
+            {
+                txtLog.Text = "";
+            }
+            txtLog.AppendText(formatted);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            courseCode = txtCourseCode.Text;
+            studentID = txtStudentId.Text;
+            label1.Hide();
+            label2.Hide();
+            label3.Hide();
+            txtStudentId.Hide();
+            txtCourseCode.Hide();
+            txtEmail.Hide();
+            webBrowser1.Hide();
+            button1.Hide();
+            txtLog.Show();
+            LogWrite("--- BOT ACTIVATED ---");
+            webBrowser1.Navigate("https://quest.pecs.uwaterloo.ca/psc/SS/ACADEMIC/SA/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART&Action=A&ACAD_CAREER=UG&EMPLID=" + studentID + "&ENRL_REQUEST_ID=&INSTITUTION=UWATR&STRM=1141");
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            String code = webBrowser1.DocumentText;
+            int index = code.IndexOf(courseCode);
+
+            if (index < 0)
+            {
+                LogWrite("ERROR - Couldn't find " + courseCode + " on page - try to restart the application and check your information or make sure you're on the correct page!");
+            }
+            else
+            {
+                code = code.Substring(index);
+                index = code.IndexOf("STATUS_CLOSED");
+                if (index < 0)
+                {
+                        //Somehow communicate that you found it
+                        Send_Success_Mail();
+                        MessageBox.Show("THE STATUS IS OPEN; DO IT NOW!!!");
+                }
+                else
+                {
+                    LogWrite("STILL CLOSED - RETRYING");
+                    //REFRESH
+                    webBrowser1.Navigate("https://quest.pecs.uwaterloo.ca/psc/SS/ACADEMIC/SA/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?Page=SSR_SSENRL_CART&Action=A&ACAD_CAREER=UG&EMPLID=" + studentID + "&ENRL_REQUEST_ID=&INSTITUTION=UWATR&STRM=1141");
+                    //WAIT 3 SECONDS
+                    timer1.Start();
+                    //TRY AGAIN
+                }
+            }
+        }
+
+        private void Send_Success_Mail()
+        {
+            var fromAddress = new MailAddress("swagbot30001@gmail.com", "COURSE CHECKER"); //this account will probably get stolen so you might want to put in your own gmail information and not upload it anywhere
+            var toAddress = new MailAddress(txtEmail.Text, "To Whomever");
+            const string fromPassword = "swagbottemp";
+            const string body = "NOW OPEN!!";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+                Timeout = 20000
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = courseCode,
+                Body = body
+            })
+            {
+                try
+                {
+                    smtp.Send(message);
+                }
+                catch (Exception e)
+                {
+                    LogWrite("EMAIL -- COULDN'T SEND");
+                    LogWrite(e.Message);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("HOW TO USE:" + Environment.NewLine + "This program is for the University of Waterloo's Quest website. Add the course to your shopping cart in enroll and go to the page Enroll -> Add. Then type in the course code and your student ID and click activate!");
+        }
+    }
+}
